a16f1f99dc9b88f4983476d89a65d82f9c5c8a73
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-01 01:23:35 +0530

    feat: added footer section to homepage

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,6 +1,62 @@
 import "./Footer.scss";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
+
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">About</div>
+          <div className="tex">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+            dolore sapiente maiores at quisquam ea sit error itaque esse quis.
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Contact</div>
+          <div className="c-item">
+            <FaLocationArrow />
+            <div className="text">C-32 Noida UP, 201301</div>
+          </div>
+          <div className="c-item">
+            <FaMobileAlt />
+            <div className="text">Phone: 0471 272 0261</div>
+          </div>
+          <div className="c-item">
+            <FaEnvelope />
+            <div className="text">Email: store@prajwal.dev</div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Categories</div>
+          <div className="text">Headphones</div>
+          <div className="text">Smart Phones</div>
+          <div className="text">Bluetooth Speakers</div>
+          <div className="text">Wireless Earbuds</div>
+          <div className="text">Home Theatre</div>
+          <div className="text">Projectors</div>
+        </div>
+        <div className="col">
+          <div className="title">Pages</div>
+          <div className="text">Home</div>
+          <div className="text">About</div>
+          <div className="text">Privacy Policy</div>
+          <div className="text">Returns</div>
+          <div className="text">Terms & Conditions</div>
+          <div className="text">Contact Us</div>
+        </div>
+      </div>
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <div className="text">
+            E-Commerce Store 2023(Multi Billion Dollar)
+          </div>
+          <img src={Payment} alt="payment-img" />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
