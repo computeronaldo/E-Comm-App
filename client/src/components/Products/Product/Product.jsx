e50923d63af316ac85841302607ae33c046eddfb
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-05 12:31:11 +0530

    implemented Products and individual product component

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,6 +1,18 @@
 import "./Product.scss";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 const Product = () => {
-    return <div>Product</div>;
+  return (
+    <div className="product-card">
+      <div className="thumbnail">
+        <img src={prod} alt="product-img" />
+      </div>
+      <div className="prod-details">
+        <span className="name">Product Name</span>
+        <span className="pric">$ 4.99</span>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
