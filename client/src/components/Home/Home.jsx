6d819e9e1a4ca40cb57f37b222980af167a921be
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-06 22:31:46 +0530

    modify: modified Home component to fetch products and categories data from API

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,37 @@
+import { useEffect, useContext } from "react";
+import { fetchDataFromApi } from "../../utils/api";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { Context } from "../../utils/context";
 import "./Home.scss";
 
 const Home = () => {
+  const { categories, setCategories, products, setProducts } =
+    useContext(Context);
+  useEffect(() => {
+    getCategories();
+    getPeoducts();
+  }, []);
+
+  const getCategories = () => {
+    fetchDataFromApi("/api/categories?populate=*").then((res) => {
+      setCategories(res);
+    });
+  };
+
+  const getPeoducts = () => {
+    fetchDataFromApi("/api/products?populate=*").then((res) => {
+      setProducts(res);
+    });
+  };
   return (
     <div>
       <Banner />
       <div className="main-content">
         <div className="layout">
-          <Category />
-          <Products />
+          <Category categories={categories} />
+          <Products products={products} />
         </div>
       </div>
     </div>
