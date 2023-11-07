d5abd05f71b2db2b651f8751d0f9707ef2f07006
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-07 23:07:57 +0530

    feat: implemented Category page

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,12 +1,25 @@
-import "./Category.scss";
+import { useParams } from "react-router-dom";
 import Products from "../Products/Products";
+import useFetch from "../../hooks/useFetch";
+import "./Category.scss";
 
 const Category = () => {
+  const { id } = useParams();
+  const { data: productsData } = useFetch(
+    `/api/products?populate=*&[filters][categories][id]=${id}`
+  );
+
+  const { data: categoryData } = useFetch(`/api/categories/${id}`);
+
   return (
     <div className="category-main-content">
       <div className="layout">
-        <div className="category-title">Category Title</div>
-        <Products innerPage={true} />
+        <div className="category-title">
+          {categoryData?.data?.attributes?.title}
+        </div>
+        {productsData && (
+          <Products innerPage={true} products={productsData.data} />
+        )}
       </div>
     </div>
   );
