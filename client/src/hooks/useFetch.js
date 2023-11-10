96092cb2ec2db888a878afa1f1ad28c1bd4d94ff
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-10 14:09:58 +0530

    refact: moved api calling function inside useEffect

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -4,14 +4,15 @@ const useFetch = (endpoint) => {
   const [data, setData] = useState();
 
   useEffect(() => {
+    const makeApiCall = async () => {
+      const res = await fetchDataFromApi(endpoint);
+      setData(res);
+    };
+
     makeApiCall();
   }, [endpoint]);
 
-  const makeApiCall = async () => {
-    const res = await fetchDataFromApi(endpoint);
-    setData(res);
-  };
-
   return { data };
 };
+
 export default useFetch;
