64e740e7b7185584b23e6301e8ad1bdd7e7206a2
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-15 21:05:56 +0530

    feat: added cart functionality to Cart and CartItem component to render data dynamically instead of being static

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { Context } from "../../utils/context";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import CartItem from "../Cart/CartItem/CartItem";
@@ -5,6 +7,7 @@ import CartItem from "../Cart/CartItem/CartItem";
 import "./Cart.scss";
 
 const Cart = ({ setShowCart }) => {
+  const { cartState } = useContext(Context);
   return (
     <div className="cart-panel">
       <div className="opac-layer"></div>
@@ -16,17 +19,21 @@ const Cart = ({ setShowCart }) => {
             <span className="text">close</span>
           </span>
         </div>
-        {/* <div className="empty-cart">
-          <BsCartX />
-          <span>No products in the cart.</span>
-          <button className="return-cta">RETURN TO CART</button>
-        </div> */}
+        {cartState.cart.length === 0 && (
+          <div className="empty-cart">
+            <BsCartX />
+            <span>No products in the cart.</span>
+            {/* <button className="return-cta">RETURN TO CART</button> */}
+          </div>
+        )}
         <>
-          <CartItem />
+          {cartState.cart.map((item) => {
+            return <CartItem key={item.id} item={item} />;
+          })}
           <div className="cart-footer">
             <div className="subtotal">
               <span className="text">Subtotal:</span>
-              <span className="text total">&#8377;1234</span>
+              <span className="text total">&#8377;{cartState.subTotal}</span>
             </div>
             <div className="button">
               <button className="checkout-cta">Checkout</button>
--- client/src/components/Cart/CartItem/CartItem.jsx
+++ client/src/components/Cart/CartItem/CartItem.jsx
@@ -1,26 +1,70 @@
-import prod from "../../../assets/products/earbuds-prod-1.webp";
 import { MdClose } from "react-icons/md";
+import { Context } from "../../../utils/context";
+import { useContext } from "react";
 import "./CartItem.scss";
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
+  const { dispatchFn } = useContext(Context);
+
+  const handleAddition = () => {
+    const productId = item.id;
+    const productName = item.name;
+    const productPrice = item.price;
+    const productImgPath = item.imgPath;
+    const productQuantity = 1;
+    dispatchFn({
+      type: "ADD_TO_CART",
+      payload: {
+        cartItem: {
+          id: productId,
+          name: productName,
+          price: productPrice,
+          imgPath: productImgPath,
+          quantity: productQuantity,
+        },
+      },
+    });
+  };
+
+  const handleRemoval = (removeAll = false) => {
+    const productId = item.id;
+    const productName = item.name;
+    const productPrice = item.price;
+    const productImagePath = item.imgPath;
+    const productQuantity = removeAll ? item.quantity : 1;
+
+    dispatchFn({
+      type: "REMOVE_FROM_CART",
+      payload: {
+        cartItem: {
+          id: productId,
+          name: productName,
+          price: productPrice,
+          imgPath: productImagePath,
+          quantity: productQuantity,
+        },
+      },
+    });
+  };
+
   return (
     <div className="cart-products">
       <div className="cart-product">
         <div className="img-container">
-          <img src={prod} alt="product-img" />
+          <img src={`http://localhost:1337${item.imgPath}`} alt="product-img" />
         </div>
         <div className="prod-details">
-          <span className="name">Product name</span>
-          <MdClose className="close-btn" />
+          <span className="name">{item.name}</span>
+          <MdClose className="close-btn" onClick={() => handleRemoval(true)} />
           <div className="quantity-buttons">
-            <span>-</span>
-            <span>5</span>
-            <span>+</span>
+            <span onClick={() => handleRemoval()}>-</span>
+            <span>{item.quantity}</span>
+            <span onClick={() => handleAddition()}>+</span>
           </div>
           <div className="text">
-            <span>3</span>
+            <span>{item.quantity}</span>
             <span>x</span>
-            <span className="highlight">&#8377;4567</span>
+            <span className="highlight">&#8377;{item.price}</span>
           </div>
         </div>
       </div>
