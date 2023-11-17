f5cf4e2f25edfaaeabaf6dddc1ab20b266f1dfa7
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-11-17 21:34:44 +0530

    feat: added search functionality to search bar based on item name

--- client/src/components/Header/Search/Search.jsx
+++ client/src/components/Header/Search/Search.jsx
@@ -1,12 +1,35 @@
-import prod from "../../../assets/products/earbuds-prod-1.webp";
+import { useState } from "react";
 import { MdClose } from "react-icons/md";
 import "./Search.scss";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
 
 const Search = ({ setShowSearchBar }) => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  let { data } = useFetch(
+    `/api/products?populate=*&filters[title][$contains]=${query}`
+  );
+
+  if (!query.length) {
+    data = null;
+  }
+
   return (
     <div className="search-modal">
       <div className="form-field">
-        <input type="text" autoFocus placeholder="Search for products" />
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for products"
+          value={query}
+          onChange={onChange}
+        />
         <MdClose
           onClick={() => {
             setShowSearchBar(false);
@@ -15,15 +38,29 @@ const Search = ({ setShowSearchBar }) => {
       </div>
       <div className="search-result-content">
         <div className="search-results">
-          <div className="search-result-item">
-            <div className="img-container">
-              <img src={prod} alt="product-img" />
-            </div>
-            <div className="prod-details">
-              <span className="name">Product name</span>
-              <span className="desc">Product desc</span>
-            </div>
-          </div>
+          {data?.data?.map((item) => {
+            return (
+              <div
+                key={item?.id}
+                className="search-result-item"
+                onClick={() => {
+                  navigate("/product/" + item.id);
+                  setShowSearchBar(false);
+                }}
+              >
+                <div className="img-container">
+                  <img
+                    src={`http://localhost:1337${item?.attributes?.img?.data[0]?.attributes?.formats?.small?.url}`}
+                    alt="product-img"
+                  />
+                </div>
+                <div className="prod-details">
+                  <span className="name">{item?.attributes?.title}</span>
+                  <span className="desc">{item?.attributes?.desc}</span>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
