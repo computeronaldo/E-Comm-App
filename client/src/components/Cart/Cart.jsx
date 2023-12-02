a55c540ea2c5b63a7a8fb8d5ae7aae7b0d1b8889
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-12-02 19:59:51 +0530

    modify: implemented onClick handler to checkout button to redirect to stripe payment gateway

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -3,11 +3,33 @@ import { Context } from "../../utils/context";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import CartItem from "../Cart/CartItem/CartItem";
+import { makePaymentRequest } from "../../utils/api";
 
 import "./Cart.scss";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = ({ setShowCart }) => {
   const { cartState } = useContext(Context);
+
+  const stripePromise = loadStripe(
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makePaymentRequest.post("/api/orders", {
+        products: cartState.cart,
+      });
+
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="cart-panel">
       <div className="opac-layer"></div>
@@ -36,7 +58,9 @@ const Cart = ({ setShowCart }) => {
               <span className="text total">&#8377;{cartState.subTotal}</span>
             </div>
             <div className="button">
-              <button className="checkout-cta">Checkout</button>
+              <button className="checkout-cta" onClick={handlePayment}>
+                Checkout
+              </button>
             </div>
           </div>
         </>
