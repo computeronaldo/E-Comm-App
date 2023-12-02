410758f7c6c5489574d8e8f9e2923d366d8fbb5b
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-12-02 20:03:51 +0530

    feat: implemented makePayment utility function to post cart information to /api/orders

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -18,3 +18,10 @@ export const fetchDataFromApi = async (url) => {
     return error;
   }
 };
+
+export const makePaymentRequest = axios.create({
+  baseURL: process.env.REACT_APP_DEV_URL,
+  headers: {
+    Authorization: "bearer " + process.env.REACT_APP_STRIPE_APP_KEY,
+  },
+});
